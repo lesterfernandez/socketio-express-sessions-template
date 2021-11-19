52953c38dab214d164dd3d93d42bb360a5d6e5c9
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-19 15:05:01 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
+const { Server } = require("socket.io");
+const indexRouter = require("./routers/indexRouter");
+const passport = require("passport");
+const session = require("express-session");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+const app = express();
+const server = require("http").createServer(app);
+
+const sessionMiddleware = session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false,
+});
+
+// Http connection(s)
+
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 
-app.listen(4000, () => {
-  console.log("Server started on port 4000");
+// rest endpoints
+app.use("/api", indexRouter);
+
+// Socket connection
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+
+io.use(wrap(sessionMiddleware));
+io.use(wrap(passport.initialize));
+io.use(wrap(passport.session));
+
+io.on("connect", socket => {});
+
+// start server
+server.listen(4000, () => {
+  console.log("Server up on port 4000");
 });
